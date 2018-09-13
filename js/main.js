a70cf6ecb6544745d2d3c9de971df9b97d9fe05c
Darrin Webster <Darrinmansa@gmail.com>
2018-09-13 01:02:00 -0700

    Second commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//console.log("connected");
+
+//Primitive data types
+
+//1. NUMBERS
+//	8
+//	100
+//	5.379
+
+//2. Strings (numbers read as strings wont work as numeric values)
+//	"connected"
+//	"This is a String"
+//	"D"
+//	"7"
+
+//3. BOOLEANS
+//	true
+//	false
+
+//4. & 5
+	//null
+	//undefined //error (undefined is an error is javascript)
+
+//variables 
+
+//var myVariable;			//declared variable
+//	myVariable = 100;	//assigned value to variable
+
+//varible delared and assigned a value all at once
+//var myNewVar = "this is a new variable";
+
+//var num = 8;
+
+var firstName = "Darrin";
+var lastName =	"Webster";
